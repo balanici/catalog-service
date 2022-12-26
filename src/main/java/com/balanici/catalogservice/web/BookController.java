a12d8e6abdb008ee723721384c1f93c2c0fb8f40
Igor Balanici <igor.balanici@gmail.com>
2022-12-27 01:51:48 +0200

    3.3.3 Add Validatio API

--- src/main/java/com/balanici/catalogservice/web/BookController.java
+++ src/main/java/com/balanici/catalogservice/web/BookController.java
@@ -1,17 +1,10 @@
 package com.balanici.catalogservice.web;
 
-import com.balanici.catalogservice.service.BookService;
 import com.balanici.catalogservice.domain.Book;
+import com.balanici.catalogservice.service.BookService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
@@ -35,7 +28,7 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Book addBookToCatalog(@RequestBody Book book) {
+    public Book addBookToCatalog(@Valid @RequestBody Book book) {
         return bookService.addBookToCatalog(book);
     }
 
@@ -46,7 +39,7 @@ public class BookController {
     }
 
     @PutMapping("{isbn}")
-    public Book editBook(@PathVariable String isbn, @RequestBody Book book) {
+    public Book editBook(@PathVariable String isbn, @Valid @RequestBody Book book) {
         return bookService.editBookDetails(isbn, book);
     }
 
