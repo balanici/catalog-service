d6e8b20bd6872b8cbc958d611050a0dd4cad8999
Igor Balanici <igor.balanici@gmail.com>
2023-01-06 14:59:06 +0200

    5.2.4 Data repositories with Spring Data

--- src/main/java/com/balanici/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/balanici/catalogservice/demo/BookDataLoader.java
@@ -2,6 +2,7 @@ package com.balanici.catalogservice.demo;
 
 import com.balanici.catalogservice.domain.Book;
 import com.balanici.catalogservice.repository.BookRepository;
+import java.util.List;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
@@ -19,9 +20,11 @@ public class BookDataLoader {
 
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
+        bookRepository.deleteAll();
         Book book1 = Book.of("1234567891", "Nothern Lights", "Lyra Silverstar", 9.90);
         Book book2 = Book.of("1234567892", "Polar Journey", "Igorek Polarson", 12.90);
         bookRepository.save(book1);
         bookRepository.save(book2);
+        bookRepository.saveAll(List.of(book1, book2));
     }
 }
--- src/main/java/com/balanici/catalogservice/repository/BookRepository.java
+++ src/main/java/com/balanici/catalogservice/repository/BookRepository.java
@@ -2,17 +2,20 @@ package com.balanici.catalogservice.repository;
 
 import com.balanici.catalogservice.domain.Book;
 import java.util.Optional;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface BookRepository {
-
-    Iterable<Book> findAll();
+public interface BookRepository extends CrudRepository<Book, Long> {
 
     Optional<Book> findByIsbn(String isbn);
 
     boolean existsByIsbn(String isbn);
 
-    Book save(Book book);
-
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM book WHERE isbn = :isbn")
     void deleteByIsbn(String isbn);
 
 }
